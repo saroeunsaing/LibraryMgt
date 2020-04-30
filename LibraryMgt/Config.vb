ce6fae5c6c20735b17db8ed819b85da6e0a8aeae
SAROEUN <SAROEUN@ACER-PC>
2020-04-30 12:53:29 +0700

    create class connection and add data to database

--- LibraryMgt/Config.vb
+++ LibraryMgt/Config.vb
@@ -1,3 +1,18 @@
-﻿Module Config
+﻿Imports System.Data.SqlClient
+Module Config
+    'Server Configuration
+    Public datasource As String = "ACER-PC"
+    Public database As String = "LibraryMgt"
+    Public user_ID As String = "sa"
+    Public pwd As String = "123"
+
+    'Global Variable Declaration
+    Public cn As SqlConnection
+    Public cmd As SqlCommand
+    Public da As SqlDataAdapter
+    Public dr As SqlDataReader
+    Public dt As New DataTable
+    Public ds As New DataSet
+    Public cs As String
 
 End Module
---
+++ LibraryMgt/ConnectionString.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Data.SqlClient
+
+Public MustInherit Class ConnectionString
+
+    ' create connection in mustinherit class to use all in project
+    Public Sub New()
+        cs = "Data Source=" + datasource + ";Initial Catalog=" + database + "; User ID=" + user_ID + ";Password=" + pwd
+
+    End Sub
+
+    Public Function GetConnection() As SqlConnection
+        Return New SqlConnection(cs)
+    End Function
+End Class
