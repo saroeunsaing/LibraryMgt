8e8d6d45cc5a80940d2e075f8791961b7607a484
SAROEUN <SAROEUN@ACER-PC>
2020-04-30 15:57:48 +0700

    Dashboard

---
+++ LibraryMgt/Frm_Dashboard.vb
@@ -0,0 +1,9 @@
+﻿Public Class Frm_Dashboard
+    Private Sub Frm_Dashboard_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Panel_Title_Paint(sender As Object, e As PaintEventArgs) Handles Panel_Title.Paint
+
+    End Sub
+End Class
